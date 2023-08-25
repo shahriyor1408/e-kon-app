4584a2854c2f568de1eb59e70c57209399dd8a8b
DilshodbekOtajonov <dilshodbekotajonov95@gmail.com>
2023-08-25 17:25:03 +0500

    commit message

--- src/main/java/com/company/proxyproject/config/SecurityConfig.java
+++ src/main/java/com/company/proxyproject/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
             "/authenticate",
+            "/documentation/**",
             "/swagger-resources/**",
             "/swagger-ui/**",
             "/v3/api-docs",
